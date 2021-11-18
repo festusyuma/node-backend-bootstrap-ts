939e4c0765c0ad490a6e11c3ace2c7f9c1031ef5
festus <festusyuma@gmail.com>
2021-11-18 15:24:15 +0100

    updates

--- src/app/repo/index.ts
+++ src/app/repo/index.ts
@@ -3,6 +3,16 @@ export const handleError = (e: any) => {
   console.error(e)
 }
 
+export const repo = async (func: () => any) => {
+  try {
+    return await func()
+  } catch (e) {
+    handleError(e)
+  }
+
+  return null
+}
+
 export {
 
 }
--- src/app/service/index.ts
+++ src/app/service/index.ts
@@ -1,6 +1,17 @@
+import response from "../util/response";
+import { ServiceResponse } from "../dto/response";
+
 export const handleError = (e: any) => {
   console.error('Service error')
   console.error(e)
+}
+
+export const service = async (func: () => Promise<ServiceResponse>) => {
+  try {
+    return await func()
+  } catch (e) {
+    return response.serverError(e)
+  }
 }
 
 export {
